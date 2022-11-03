98e5930b0852c7da6cc833d94bc98f4d094bcca2
JustynaSobczak2000 <jsob605@gmail.com>
2022-11-03 19:59:42 +0100

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,64 +1,45 @@
-const { clear } = require('i/lib/inflections');
-const _ = require('lodash');
+const throttle = require('lodash.throttle');
 
-// html tags
-const input = document.querySelector(`input[type="email"]`);
-const textarea = document.querySelector(`textarea[name="message"]`);
-const button = document.querySelector(`button[type="submit"]`);
-const form = document.querySelector('feedback-form');
-// html tags
+const input = document.querySelector('input');
+const textarea = document.querySelector('textarea');
+const form = document.querySelector('.feedback-form');
+const submitBtn = document.querySelector('button');
 
-//object and local storage
-const obj = {
-  email: '',
-  textarea: '',
-};
-const load = localStorage.getItem('feedback-form-state');
-const parsedLoad = JSON.parse(load);
-//object and local storage
+form.addEventListener('input', throttle(inputData, 500));
 
-//callbacks and listeners for email and  for message
-const callback1 = () => {
-  obj.email = input.value;
-  localStorage.setItem('feedback-form-state', JSON.stringify(obj));
-};
+function inputData() {
+  const data = {
+    email: input.value,
+    message: textarea.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(data));
+}
 
-const callback2 = () => {
-  obj.textarea = textarea.value;
-  localStorage.setItem('feedback-form-state', JSON.stringify(obj));
-};
-input.addEventListener('input', _.debounce(callback1, 500));
-textarea.addEventListener('input', _.debounce(callback2, 500));
-//callbacks and listeners for email and  for message
+if (localStorage.length !== 0) {
+  input.value = JSON.parse(localStorage.getItem('feedback-form-state')).email;
+  textarea.value = JSON.parse(
+    localStorage.getItem('feedback-form-state')
+  ).message;
+}
+// else {
+//   input.innerText = '';
+//   textarea.innerText = '';
+// }
 
-//submit event
-const handleSubmit = evt => {
-  evt.preventDefault();
-  if (input.value === '' || textarea.value === '') {
-    return alert('please fill all the fields!');
-  } else if (localStorage.length === 0) {
-    return;
-  } else if (parsedLoad !== '') {
-    obj.email = input.value;
-    obj.textarea = textarea.value;
-    console.log(obj);
-    console.log(localStorage);
-    localStorage.clear();
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, message },
+  } = event.currentTarget;
+
+  if (email.value === '' || message.value === '') {
+    return alert('Proszę uzupełnić wszystkie pola!');
+  } else {
+    const result = { email: email.value, message: message.value };
+    console.log(result);
+    event.currentTarget.reset();
+    localStorage.removeItem('feedback-form-state');
   }
-};
-button.addEventListener('click', handleSubmit);
-//submit event
-
-//reload page
-const check = () => {
-  if (localStorage === null) {
-    return;
-  } else if (localStorage.getItem('feedback-form-state') === null) {
-    return;
-  } else if (parsedLoad.email !== null || parsedLoad.textarea !== null) {
-    input.value = parsedLoad.email;
-    textarea.value = parsedLoad.textarea;
-  }
-};
-window.addEventListener('load', check);
-//reload page
+}
